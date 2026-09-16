907430a1461f5cf2f00f689ec3ff83e3f564770e
YouJez <158460279+YouJez@users.noreply.github.com>
2026-09-16 03:28:23 -0700

    added dotnet
    -added controller
    -added service

---
+++ MedicineInventory.Api/Controllers/TestController.cs
@@ -0,0 +1,28 @@
+﻿using MedicineInventory.Api.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MedicineInventory.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class TestController : ControllerBase
+{
+    private readonly ITestService _testService;
+
+    public TestController(ITestService testService)
+    {
+        _testService = testService;
+    }
+
+    [HttpGet]
+    public IActionResult Get()
+    {
+        var message = _testService.GetMessage();
+
+        return Ok(new
+        {
+            success = true,
+            message = message
+        });
+    }
+}
---
+++ MedicineInventory.Api/Services/TestService.cs
@@ -0,0 +1,14 @@
+﻿namespace MedicineInventory.Api.Services;
+
+public interface ITestService
+{
+    string GetMessage();
+}
+
+public class TestService : ITestService
+{
+    public string GetMessage()
+    {
+        return "Hello from MedicineInventory API!";
+    }
+}
